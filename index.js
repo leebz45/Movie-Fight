4936f09425f8486d64e188603d1f13f033e01891
leebz45 <43051856+leebz45@users.noreply.github.com>
2022-04-07 20:43:44 -0400

    Add search logic

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com',{
+        params: {
+            apikey: Config.apikey,
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+ 
+
+const onInput = debounce(event => {
+  fetchData(event.target.value);
+}, 500);
+
+input.addEventListener('input',onInput);
