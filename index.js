a74b06aa942e3c5e5550ce9bfb189f389cfd3b8e
Matt Leeber <43051856+leebz45@users.noreply.github.com>
2022-04-08 11:57:11 -0400

    Add comparisson logic

--- index.js
+++ index.js
@@ -61,7 +61,23 @@ const onMovieSelect = async (movie, summaryElement,side) => {
 };
 
 const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
 
+    leftSideStats.forEach((leftStat,index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if(rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else if(leftSideValue > rightSideValue) {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
+    });
 };
 
 const movieTemplate = (movieDetail) => {
